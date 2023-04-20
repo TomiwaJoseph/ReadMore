550a3a87f9466307af4b551147bd2431b210f7ed
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-04-19 22:06:11 -0700

    first commit

---
+++ frontend/src/pages/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import badUrl from "../static/badUrl.png";
+
+const ErrorPage = () => {
+  return (
+    <div className="error__div">
+      <div className="error_image_wrapper">
+        <img className="img-fluid" src={badUrl} alt="bad url" />
+      </div>
+      <h1>Unknown url pattern...</h1>
+      <p>
+        Don't mess with the url. It might take you to where you don't know 😅
+      </p>
+      <Link className="single__page-error" to="/shop">
+        Go Back To Shop
+      </Link>
+    </div>
+  );
+};
+
+export default ErrorPage;
---
+++ frontend/src/pages/NoInternet.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoInternet = () => {
+  return (
+    <div>
+      <h1>No Internet</h1>
+    </div>
+  );
+};
+
+export default NoInternet;
