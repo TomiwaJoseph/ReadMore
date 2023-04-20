a3a317706fd2e8d4ab01edeeff090dc4f7ff33e6
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-04-19 22:06:31 -0700

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,126 @@
+import img1 from "./static/products/product-item1.jpg";
+import img2 from "./static/products/product-item2.jpg";
+import img3 from "./static/products/product-item3.jpg";
+import img4 from "./static/products/product-item4.jpg";
+import img5 from "./static/products/product-item5.jpg";
+import img6 from "./static/products/product-item6.jpg";
+import img7 from "./static/products/product-item7.jpg";
+import img8 from "./static/products/product-item8.jpg";
+import img9 from "./static/products/product-item9.jpg";
+
+export const featuredProducts = [
+  {
+    name: "Simple Way Of Piece Life",
+    slug: "simple-way-of-piece-life",
+    image: img1,
+    author: "Armor Ramsey",
+    price: "40.00",
+  },
+  {
+    name: "Great Travel At Desert",
+    slug: "great-travel-at-desert",
+    image: img2,
+    author: "Sanchit Howdy",
+    price: "38.00",
+  },
+  {
+    name: "The Lady Beauty Scarlett",
+    slug: "the-lady-beauty-scarlett",
+    image: img3,
+    author: "Arthur Doyle",
+    price: "45.00",
+  },
+  {
+    name: "Once Upon A Time",
+    slug: "once-upon-a-time",
+    image: img4,
+    author: "Klien Marry",
+    price: "35.00",
+  },
+];
+
+export const quotes = [
+  {
+    quote:
+      "Reading is essential for those who seek to rise above the ordinary.",
+    author: "Jim Rohn",
+  },
+  {
+    quote: "Think before you speak. Read before you think.",
+    author: "Fran Lebowitz",
+  },
+  {
+    quote: "A room without books is like a body without a soul.",
+    author: "Cicero",
+  },
+  {
+    quote: "A book is a gift you can open again and again.",
+    author: "Garrison Keillor",
+  },
+];
+
+export const offerProducts = [
+  {
+    name: "Once Upon A Time",
+    slug: "once-upon-a-time",
+    image: img9,
+    author: "Klien Marry",
+    price: "35.00",
+  },
+  {
+    name: "Does To Me",
+    slug: "once-upon-a-time",
+    image: img7,
+    author: "Orlando Owoh",
+    price: "25.00",
+  },
+  {
+    name: "Simple Way Of Piece Life",
+    slug: "simple-way-of-piece-life",
+    image: img1,
+    author: "Armor Ramsey",
+    price: "40.00",
+  },
+  {
+    name: "Great Travel At Desert",
+    slug: "great-travel-at-desert",
+    image: img2,
+    author: "Sanchit Howdy",
+    price: "38.00",
+  },
+  {
+    name: "The Lady Beauty Scarlett",
+    slug: "the-lady-beauty-scarlett",
+    image: img3,
+    author: "Arthur Doyle",
+    price: "45.00",
+  },
+  {
+    name: "Once Upon A Time",
+    slug: "once-upon-a-time",
+    image: img4,
+    author: "Klien Marry",
+    price: "35.00",
+  },
+  {
+    name: "Simple Way Of Piece Life",
+    slug: "simple-way-of-piece-life",
+    image: img5,
+    author: "Armor Ramsey",
+    price: "40.00",
+  },
+  {
+    name: "Great Travel At Desert",
+    slug: "great-travel-at-desert",
+    image: img6,
+    author: "Sanchit Howdy",
+    price: "38.00",
+  },
+  {
+    name: "The Lady Beauty Scarlett",
+    slug: "the-lady-beauty-scarlett",
+    image: img8,
+    author: "Arthur Doyle",
+    price: "45.00",
+  },
+];
