996f3388b4d1653d457e2ab279a623eee29d5179
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-04-21 14:59:00 -0700

    first commit

---
+++ frontend/src/components/Preloader.jsx
@@ -0,0 +1,37 @@
+import "./preloader.css";
+
+const Preloader = () => {
+  return (
+    <div className="preloader-container">
+      <div className="book">
+        <div className="inner">
+          <div className="left"></div>
+          <div className="middle"></div>
+          <div className="right"></div>
+        </div>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
