d0f436721e1502f7863f7d5bd6af229fffc46d21
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-04-22 04:03:35 -0700

    added more static data

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -7,6 +7,19 @@ import img6 from "./static/products/product-item6.jpg";
 import img7 from "./static/products/product-item7.jpg";
 import img8 from "./static/products/product-item8.jpg";
 import img9 from "./static/products/product-item9.jpg";
+import img10 from "./static/products/single-image.jpg";
+import img11 from "./static/products/tab-item1.jpg";
+import img12 from "./static/products/tab-item3.jpg";
+import img13 from "./static/products/tab-item4.jpg";
+import img14 from "./static/products/tab-item5.jpg";
+import img15 from "./static/products/tab-item7.jpg";
+
+import cat1 from "./static/cat1.png";
+import cat2 from "./static/cat2.png";
+import cat3 from "./static/cat3.png";
+import cat4 from "./static/cat4.png";
+import cat5 from "./static/cat5.png";
+import cat6 from "./static/cat6.png";
 
 export const featuredProducts = [
   {
@@ -122,5 +135,174 @@ export const offerProducts = [
     image: img8,
     author: "Arthur Doyle",
     price: "45.00",
+  },
+];
+
+export const allProducts = [
+  // {
+  //   name: "Once Upon A Time",
+  //   slug: "once-upon-a-time",
+  //   image: img9,
+  //   author: "Klien Marry",
+  //   price: "35.00",
+  // },
+  {
+    name: "Does To Me",
+    slug: "once-upon-a-time",
+    image: img7,
+    author: "Orlando Owoh",
+    price: "25.00",
+  },
+  {
+    name: "Simple Way Of Piece Life",
+    slug: "simple-way-of-piece-life",
+    image: img1,
+    author: "Armor Ramsey",
+    price: "40.00",
+  },
+  {
+    name: "Great Travel At Desert",
+    slug: "great-travel-at-desert",
+    image: img2,
+    author: "Sanchit Howdy",
+    price: "38.00",
+  },
+  {
+    name: "The Lady Beauty Scarlett",
+    slug: "the-lady-beauty-scarlett",
+    image: img3,
+    author: "Arthur Doyle",
+    price: "45.00",
+  },
+  {
+    name: "Once Upon A Time",
+    slug: "once-upon-a-time",
+    image: img4,
+    author: "Klien Marry",
+    price: "35.00",
+  },
+  {
+    name: "Simple Way Of Piece Life",
+    slug: "simple-way-of-piece-life",
+    image: img5,
+    author: "Armor Ramsey",
+    price: "40.00",
+  },
+  {
+    name: "Great Travel At Desert",
+    slug: "great-travel-at-desert",
+    image: img6,
+    author: "Sanchit Howdy",
+    price: "38.00",
+  },
+  {
+    name: "The Lady Beauty Scarlett",
+    slug: "the-lady-beauty-scarlett",
+    image: img8,
+    author: "Arthur Doyle",
+    price: "45.00",
+  },
+  // {
+  //   name: "The Lady Beauty Scarlett",
+  //   slug: "the-lady-beauty-scarlett",
+  //   image: img10,
+  //   author: "Arthur Doyle",
+  //   price: "45.00",
+  // },
+  {
+    name: "Once Upon A Time",
+    slug: "once-upon-a-time",
+    image: img11,
+    author: "Klien Marry",
+    price: "35.00",
+  },
+  {
+    name: "Simple Way Of Piece Life",
+    slug: "simple-way-of-piece-life",
+    image: img12,
+    author: "Armor Ramsey",
+    price: "40.00",
+  },
+  {
+    name: "Great Travel At Desert",
+    slug: "great-travel-at-desert",
+    image: img13,
+    author: "Sanchit Howdy",
+    price: "38.00",
+  },
+  {
+    name: "Once Upon A Time",
+    slug: "once-upon-a-time",
+    image: img14,
+    author: "Klien Marry",
+    price: "35.00",
+  },
+  {
+    name: "Does To Me",
+    slug: "once-upon-a-time",
+    image: img15,
+    author: "Orlando Owoh",
+    price: "25.00",
+  },
+];
+
+export const justBooks = [
+  {
+    name: "Once Upon A Time",
+    slug: "once-upon-a-time",
+    image: img8,
+    author: "Klien Marry",
+    price: "35.00",
+  },
+  {
+    name: "Does To Me",
+    slug: "once-upon-a-time",
+    image: img7,
+    author: "Orlando Owoh",
+    price: "25.00",
+  },
+];
+
+export const justHistory = [
+  {
+    name: "Simple Way Of Piece Life",
+    slug: "simple-way-of-piece-life",
+    image: img1,
+    author: "Armor Ramsey",
+    price: "40.00",
+  },
+  {
+    name: "Great Travel At Desert",
+    slug: "great-travel-at-desert",
+    image: img2,
+    author: "Sanchit Howdy",
+    price: "38.00",
+  },
+];
+
+export const allCategoryList = [
+  {
+    name: "Textbooks",
+    image: cat1,
+  },
+  {
+    name: "Science",
+    image: cat2,
+  },
+  {
+    name: "History",
+    image: cat3,
+  },
+  {
+    name: "Biography",
+    image: cat4,
+  },
+  {
+    name: "Adventure",
+    image: cat5,
+  },
+  {
+    name: "Fantasy",
+    image: cat6,
   },
 ];
--- frontend/src/redux/reducers/bookReducer.js
+++ frontend/src/redux/reducers/bookReducer.js
@@ -3,7 +3,11 @@ import { ActionTypes } from "../actions/action-types";
 const globalState = {
   isAuthenticated: false,
   cartCount: 2,
-  backendUrl: "http://localhost:8000",
+  fetchingData: false,
+  noInternet: false,
+  highestPrice: 120,
+  currentCategoryData: [],
+  //   backendUrl: "http://localhost:8000",
 };
 
 export const bookReducer = (state = globalState, { type, payload }) => {
