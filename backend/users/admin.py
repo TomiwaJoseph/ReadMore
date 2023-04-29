7a8722e22e98e8f1506de2076999f93f1b7acaae
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-04-28 21:26:20 -0700

    added models and to admin views

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.utils import timezone
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser, Wishlist
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': (
+            'is_active', 'is_staff', 'is_superuser',
+            'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_active')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email', 'first_name', 'last_name')
+
+
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ['user']
+    list_filter = ['user']
+
+
+admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Wishlist)
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractUser
+)
+from django.conf import settings
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    """User model"""
+    username = None
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def get_name(self):
+        return super().get_full_name()
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Book(models.Model):
+    book_identifier = models.CharField(max_length=100)
+
+    def __str__(self):
+        return "{}".format(self.book_identifier)
+
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                                related_name="wishlist_owner", null=True)
+    folder = models.ManyToManyField(Book, blank=True)
+
+    def __str__(self):
+        return "{}'s wishlist".format(self.user)
