916fc5dc5432433a3876af83cb6ecd40ac19e410
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-04-30 17:36:25 -0700

    first commit

---
+++ frontend/src/pages/Cart.jsx
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+
+const Cart = () => {
+  return (
+    <div className="cart-container">
+      <h1>Cart</h1>
+    </div>
+  );
+};
+
+export default Cart;
---
+++ frontend/src/pages/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Checkout = () => {
+  return (
+    <div className="checkout-container">
+      <h1>Checkout</h1>
+    </div>
+  );
+};
+
+export default Checkout;
