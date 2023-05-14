daa2278dadf8aa9bc4a31a305eeb3c77c5df9d0d
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-05-13 22:14:22 -0700

    added api endpoint to handle stripe payment

--- frontend/src/stripe-script.js
+++ frontend/src/stripe-script.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-const API_ENDPOINT = "http://localhost:8000/api/";
+const API_ENDPOINT = "http://localhost:8000/api";
 
 export const stripePaymentMethodHandler = async (data) => {
-  const { amount, orderInfo, result } = data;
+  const { amount, result } = data;
   let body = JSON.stringify({
     payment_method_id: result.id,
     name: result.billing_details.name,
     email: result.billing_details.email,
     amount: amount,
-    orderInfo: orderInfo,
     token: localStorage.getItem("readMoreToken"),
   });
   const res = await axios.post(`${API_ENDPOINT}/save-stripe-info/`, body, {
