6bf857b5b05ca896667c33516de590208431d297
TomiwaJoseph <tomiwajoseph88@gmail.com>
2023-05-14 16:29:45 -0700

    removed unused elements

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,18 +1,9 @@
 import img1 from "./static/products/product-item1.jpg";
-import img2 from "./static/products/product-item2.jpg";
-import img3 from "./static/products/product-item3.jpg";
-import img4 from "./static/products/product-item4.jpg";
 import img5 from "./static/products/product-item5.jpg";
 import img6 from "./static/products/product-item6.jpg";
-import img7 from "./static/products/product-item7.jpg";
-import img8 from "./static/products/product-item8.jpg";
 import img9 from "./static/products/product-item9.jpg";
 import img10 from "./static/products/single-image.jpg";
 import img11 from "./static/products/tab-item1.jpg";
-import img12 from "./static/products/tab-item3.jpg";
-import img13 from "./static/products/tab-item4.jpg";
-import img14 from "./static/products/tab-item5.jpg";
-import img15 from "./static/products/tab-item7.jpg";
 
 import cat1 from "./static/cat1.png";
 import cat2 from "./static/cat2.png";
@@ -38,40 +29,6 @@ export const quotes = [
   {
     quote: "A book is a gift you can open again and again.",
     author: "Garrison Keillor",
-  },
-];
-
-export const justBooks = [
-  {
-    name: "Once Upon A Time",
-    slug: "once-upon-a-time",
-    image: img8,
-    author: "Klien Marry",
-    price: "35.00",
-  },
-  {
-    name: "Does To Me",
-    slug: "once-upon-a-time",
-    image: img7,
-    author: "Orlando Owoh",
-    price: "25.00",
-  },
-];
-
-export const justHistory = [
-  {
-    name: "Simple Way Of Piece Life",
-    slug: "simple-way-of-piece-life",
-    image: img1,
-    author: "Armor Ramsey",
-    price: "40.00",
-  },
-  {
-    name: "Great Travel At Desert",
-    slug: "great-travel-at-desert",
-    image: img2,
-    author: "Sanchit Howdy",
-    price: "38.00",
   },
 ];
 
@@ -103,120 +60,3 @@ export const allCategoryList = [
 ];
 
 export const allImages = [img1, img5, img6, img9, img10, img11];
-
-export const newTin = [
-  {
-    key: "/works/OL18816681W",
-    type: "work",
-    seed: [
-      "/books/OL34869332M",
-      "/books/OL9714951M",
-      "/books/OL34830386M",
-      "/books/OL34504890M",
-      "/works/OL18816681W",
-      "/subjects/fiction_erotica_general",
-      "/authors/OL2631956A",
-    ],
-    title: "Sexy devil",
-    title_suggest: "Sexy devil",
-    title_sort: "Sexy devil",
-    edition_count: 4,
-    edition_key: ["OL34869332M", "OL9714951M", "OL34830386M", "OL34504890M"],
-    publish_date: ["2007", "November 27, 2007"],
-    publish_year: [2007],
-    first_publish_year: 2007,
-    number_of_pages_median: 320,
-    lccn: ["2009502007"],
-    lcc: ["PS-3623.00000000.H57885 S49 2007", "PS-3623.00000000.H57885"],
-    isbn: [
-      "1306687470",
-      "0758228228",
-      "9780758283269",
-      "0758283261",
-      "0758219903",
-      "9781306687478",
-      "9780758219909",
-      "9780758228222",
-    ],
-    last_modified_i: 1673663140,
-    ebook_count_i: 0,
-    ebook_access: "no_ebook",
-    has_fulltext: false,
-    public_scan_b: false,
-    readinglog_count: 1,
-    want_to_read_count: 1,
-    currently_reading_count: 0,
-    already_read_count: 0,
-    cover_edition_key: "OL9714951M",
-    cover_i: 1416909,
-    publisher: ["Kensington Publishing Corporation", "Aphrodisia"],
-    language: ["eng"],
-    author_key: ["OL2631956A"],
-    author_name: ["Sasha White"],
-    subject: ["Fiction, erotica, general"],
-    id_goodreads: ["1149251"],
-    id_librarything: ["3349322"],
-    publisher_facet: ["Aphrodisia", "Kensington Publishing Corporation"],
-    subject_facet: ["Fiction, erotica, general"],
-    _version_: 1759758720316735500,
-    lcc_sort: "PS-3623.00000000.H57885 S49 2007",
-    author_facet: ["OL2631956A Sasha White"],
-    subject_key: ["fiction_erotica_general"],
-  },
-  {
-    key: "/works/OL15186799W",
-    type: "work",
-    seed: [
-      "/books/OL24318889M",
-      "/works/OL15186799W",
-      "/subjects/erotic_literature",
-      "/subjects/fiction",
-      "/subjects/romance",
-      "/subjects/fiction_erotica_general",
-      "/authors/OL6818836A",
-    ],
-    title: "Sexy Devil",
-    title_suggest: "Sexy Devil",
-    title_sort: "Sexy Devil",
-    edition_count: 1,
-    edition_key: ["OL24318889M"],
-    publish_date: ["2007"],
-    publish_year: [2007],
-    first_publish_year: 2007,
-    publish_place: ["New York"],
-    isbn: ["9780758228208", "0758228201", "9780758228215", "075822821X"],
-    last_modified_i: 1633693239,
-    ebook_count_i: 0,
-    ebook_access: "no_ebook",
-    has_fulltext: false,
-    public_scan_b: false,
-    cover_edition_key: "OL24318889M",
-    cover_i: 6443562,
-    publisher: ["Kensington Publishing Corp."],
-    language: ["eng"],
-    author_key: ["OL6818836A"],
-    author_name: ["Sasha White"],
-    subject: [
-      "Erotic Literature",
-      "Fiction",
-      "Romance",
-      "Fiction, erotica, general",
-    ],
-    id_overdrive: ["B0FB963D-4EC3-456C-A4C8-C0956988F4B3"],
-    publisher_facet: ["Kensington Publishing Corp."],
-    subject_facet: [
-      "Erotic Literature",
-      "Fiction",
-      "Fiction, erotica, general",
-      "Romance",
-    ],
-    _version_: 1759750369366245400,
-    author_facet: ["OL6818836A Sasha White"],
-    subject_key: [
-      "erotic_literature",
-      "fiction",
-      "fiction_erotica_general",
-      "romance",
-    ],
-  },
-];
